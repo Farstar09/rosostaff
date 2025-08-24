6437ec5a1e1a9801fb5fbeafee0260665e420077
Farstar09 <71098116+Farstar09@users.noreply.github.com>
2025-08-24 14:43:42 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,6 @@ export default defineConfig({
   plugins: [react()],
   // For GitHub Pages, set to your repo name, e.g. base: '/roso-staff-site/'
   // base: '/<your-repo-name>/',
+base: '/rosostaff/',
+
 })
